74d8ac72d67e3858f8ea48ee8bc8961e65bc4a3f
jarcelo <josepharcelo@gmail.com>
2014-10-23 14:10:09 -0500

    rename the class library and did a sample call

--- consoleRPSLS/Program.cs
+++ consoleRPSLS/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameLibrary;
+
 
 namespace consoleRPSLS
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            // sample output from main
+            Console.WriteLine("Hello, world! from Main.");
+            // sample call from game library
+            RPSLS classCall = new RPSLS();
+            classCall.SampleClassCall();
+
+            Console.ReadLine();
         }
     }
 }
