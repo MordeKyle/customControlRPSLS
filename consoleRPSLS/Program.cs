5b9712f0cf7b26e02fa9521bc81d0308deeec54d
MordeKyle <Kyle@MordeKyle.com>
2014-10-23 13:27:54 -0500

    Built gameLib and Console

--- consoleRPSLS/Program.cs
+++ consoleRPSLS/Program.cs
@@ -3,13 +3,120 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using gameLib;
 
 namespace consoleRPSLS
 {
     class Program
     {
+        public enum Choice { Rock = 0, Paper = 1, Scissors = 2, Lizard = 3, Spock = 4 }
+        public enum Outcome { Win = 10, Lose = 11, Draw = 12 }
+
         static void Main(string[] args)
         {
+
+            string userInput = "";
+            int userChoice;
+            rockPaperScissorsLizardSpock RPSLS = new rockPaperScissorsLizardSpock();
+            while (true)
+            {
+                splash();
+                userInput = Console.ReadLine();
+                userChoice = determineChoice(userInput);
+                switch (userChoice)
+                {
+                    case 5: // user selects exit
+                        if (confirm() == true)
+                        {
+                            Environment.Exit(0);
+                        }
+                        break;
+                    case 6: // user inputs invalid input
+                        Console.Clear();
+                        Console.WriteLine("***Type the number that corresponds with your selection, then press enter.***\n");
+                        break;
+                    default: // user plays the game
+                        string m_result;
+                        string m_userInput;
+                        string m_computerChoice;
+                        m_result = RPSLS.playGame(userChoice);
+                        m_userInput = RPSLS.PlayerChoice;
+                        m_computerChoice = RPSLS.ComputerChoice;
+                        displayResults(m_userInput, m_computerChoice, m_result);
+                        break;
+                }
+            }
+        }
+
+        // display user's options
+        static void splash()
+        {
+            Console.WriteLine("Please make a selection and press the return key:\n");
+            Console.WriteLine("1. Rock\n2. Paper\n3. Scissors\n4. Lizard\n5. Spock\n6. Exit Game");
+        }
+
+        // confirm user's choice
+        static Boolean confirm()
+        {
+            string m_userInput = "";
+            bool m_result;
+            Console.Clear();
+            Console.WriteLine("Are you sure?\n Y or N.");
+            m_userInput = Console.ReadLine();
+            while ((m_userInput != "y") && (m_userInput != "Y") && (m_userInput != "n") && (m_userInput != "N"))
+            {
+                Console.WriteLine("Type Y or N and hit the return key. \n");
+                m_userInput = Console.ReadLine();
+            }
+            if (m_userInput == "Y" || m_userInput == "y")
+            {
+                m_result = true;
+            }
+            else
+            {
+                m_result = false;
+            }
+            return m_result;
+        }
+
+        // assign user's input to game value
+        static int determineChoice(string m_userInput)
+        {
+            int m_result;
+            switch (m_userInput)
+            {
+                case "1":
+                    m_result = 0;
+                    break;
+                case "2":
+                    m_result = 1;
+                    break;
+                case "3":
+                    m_result = 2;
+                    break;
+                case "4":
+                    m_result = 3;
+                    break;
+                case "5":
+                    m_result = 4;
+                    break;
+                case "6":
+                    m_result = 5;
+                    break;
+                default:
+                    m_result = 6;
+                    break;
+            }
+            return m_result;
+        }
+
+        static void displayResults(string m_userChoice, string m_computerChoice, string m_gameResult)
+        {
+            Console.Clear();
+            Console.WriteLine("You chose: " + m_userChoice);
+            Console.WriteLine("The computer Chose: " + m_computerChoice);
+            Console.WriteLine(m_gameResult);
+            Console.WriteLine("\n" + "\n");
         }
     }
 }
--- gameLib/rockPaperScissorsLizardSpock.cs
+++ gameLib/rockPaperScissorsLizardSpock.cs
@@ -8,5 +8,128 @@ namespace gameLib
 {
     public class rockPaperScissorsLizardSpock
     {
+        public enum Choice {Rock = 0, Paper = 1, Scissors = 2, Lizard = 3, Spock = 4}
+        public enum Outcome {Win = 10, Lose = 11, Draw = 12}
+
+        private int playerChoice;
+        public string PlayerChoice
+        {
+            get { return displayUserChoice(playerChoice); }
+        }
+        // following commented code was not working correctly. It did not assign a new random value each time the game was played:
+        //generating random for computer's choice and outputting
+        //private static readonly Random rand = new Random();
+        //private int computerChoice = rand.Next(5);
+
+        private int computerChoice;
+        public string ComputerChoice
+        {
+            get {return displayComputerChoice(computerChoice);}
+        }
+        private int generateComputerInput()
+        {
+            int m_result;
+            Random rand = new Random();
+            m_result = rand.Next(5);
+            return m_result;
+        }
+
+        //assigning user's input to a real world value
+        public string displayUserChoice(int m_userInput)
+        {
+            string m_result = "";
+            switch (m_userInput)
+            {
+                case (int)Choice.Rock:
+                    m_result = "ROCK";
+                    break;
+                case (int)Choice.Paper:
+                    m_result = "PAPER";
+                    break;
+                case (int)Choice.Scissors:
+                    m_result = "SCISSORS";
+                    break;
+                case (int)Choice.Lizard:
+                    m_result = "LIZARD";
+                    break;
+                case (int)Choice.Spock:
+                    m_result = "SPOCK";
+                    break;
+            }
+            return m_result;
+        }
+
+        //assigning computer's choice to real world value
+        public string displayComputerChoice(int m_computerChoice)
+        {
+            string m_result = "";
+            switch (m_computerChoice)
+            {
+                case (int)Choice.Rock:
+                    m_result = "ROCK";
+                    break;
+                case (int)Choice.Paper:
+                    m_result = "PAPER";
+                    break;
+                case (int)Choice.Scissors:
+                    m_result = "SCISSORS";
+                    break;
+                case (int)Choice.Lizard:
+                    m_result = "LIZARD";
+                    break;
+                case (int)Choice.Spock:
+                    m_result = "SPOCK";
+                    break;
+            }
+            return m_result;
+        }
+
+        //assigning results to real world value
+        public string displayResult(int m_outcome)
+        {
+            string m_result = "";
+            switch (m_outcome)
+            {
+                case (int)Outcome.Win:
+                    m_result = "You Win!";
+                    break;
+                case (int)Outcome.Lose:
+                    m_result = "You Lose!";
+                    break;
+                case (int)Outcome.Draw:
+                    m_result = "Draw.";
+                    break;
+            }
+            return m_result;
+        }
+
+        //determine outcome of the game
+        public int determineOutcome(int m_playerChoice, int m_computerChoice)
+        {
+            int m_result;
+            int[,] selection = new int[5, 5]
+            {
+                {(int)Outcome.Draw,(int)Outcome.Lose,(int)Outcome.Win,(int)Outcome.Win,(int)Outcome.Lose},
+                {(int)Outcome.Win,(int)Outcome.Draw,(int)Outcome.Lose,(int)Outcome.Lose,(int)Outcome.Win},
+                {(int)Outcome.Lose,(int)Outcome.Win,(int)Outcome.Draw,(int)Outcome.Win,(int)Outcome.Lose},
+                {(int)Outcome.Lose,(int)Outcome.Win,(int)Outcome.Lose,(int)Outcome.Draw,(int)Outcome.Win},
+                {(int)Outcome.Win,(int)Outcome.Lose,(int)Outcome.Win,(int)Outcome.Lose,(int)Outcome.Draw}
+            };
+            m_result = selection[m_playerChoice, m_computerChoice];
+            playerChoice = m_playerChoice;
+            return m_result;
+        }
+        public string playGame(int m_playerChoice)
+        {
+            //string m_playerChoiceOut;
+            string m_resultOut;
+            int m_resultHolder;
+            int m_computerInput = generateComputerInput(); //generate a new random each time game is played.
+            m_resultHolder = determineOutcome(m_playerChoice, m_computerInput); //find win, lose, or draw
+            m_resultOut = displayResult(m_resultHolder); //assign a real world value to game result value
+            computerChoice = m_computerInput; //holds the computer input on a class level to be converted to real world value
+            return m_resultOut;
+
+        }
     }
 }
