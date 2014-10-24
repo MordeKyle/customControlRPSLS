c62c553b2994534b2ffc2e4dcd49fd31a2bfed90
jarcelo <josepharcelo@gmail.com>
2014-10-23 20:38:12 -0500

    added rpsls class and made the console version working

--- consoleRPSLS/Program.cs
+++ consoleRPSLS/Program.cs
@@ -13,13 +13,92 @@ namespace consoleRPSLS
 
         static void Main(string[] args)
         {
-            // sample output from main
-            Console.WriteLine("Hello, world! from Main.");
-            // sample call from game library
-            RPSLS classCall = new RPSLS();
-            classCall.SampleClassCall();
+            // Show Welcome Message
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*          Welcome to Rock, Paper, Scissors, Lizard, Spock Game!         *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("TO PLAY: Press R=Rock, P=Paper, S=Scissors, L=Lizard, K=Spock");
 
-            Console.ReadLine();
+            // Declare a variable for player input
+            string playerInput = GetInput();
+
+            do
+            {
+                // create a new game instance
+                RPSLS play = new RPSLS();
+                int player = GetPlayerChoice(playerInput);
+                play.PlayerChoice = player; // invokes set accessor
+                // get player choice
+                int playerChoice = play.PlayerChoice;
+                // validate player choice
+                if (!(Enum.IsDefined(typeof(RPSLS.Choice), playerChoice)))
+                {
+                    Console.WriteLine("Please enter a valid input!");
+                }
+                else
+                {
+                    // get computer choice (Invokes the get accessor)
+                    int computerChoice = play.ComputerChoice;
+                    
+                    // display player's choice 
+                    string showPlayerChoice = play.DisplayChoice(playerChoice);
+                    Console.WriteLine("Your Choice       : " + showPlayerChoice);
+                    // display computer choice
+                    string showComputerChoice = play.DisplayChoice(computerChoice);
+                    Console.WriteLine("Computer's Choice : " + showComputerChoice);
+                    // determine outcome
+                    int outcome = play.DetermineOutcome();
+                    // display outcome
+                    Console.WriteLine("Game Result       : " + play.DisplayOutcome(outcome));
+                }
+                // prompt if player wants to play again
+                Console.WriteLine();
+                Console.WriteLine("Play again? Press 'R'=rock, 'P'=paper, 'S'=Scissors, 'L'=Lizard, 'K'=Spock or 'N' to exit game.");
+                playerInput = GetInput();
+                // confirm exit
+                if (playerInput == "n" || playerInput == "N")
+                {
+                    Console.WriteLine("Press 'Y' to confirm exit or press 'R', 'P', 'S', 'L', 'K' to play again.");
+                    playerInput = GetInput();
+                }
+
+             } while ((playerInput != "y") && (playerInput != "Y"));
         }
+        // method to get player input (set to uppercase)
+        private static string GetInput()
+        {
+            Console.WriteLine();
+            Console.Write("Enter Choice: ");
+            string input = "";
+            input = (Console.ReadLine().ToUpper());
+            return input;
+        }
+        // Method to get player choice
+        public static int GetPlayerChoice(string choice)
+        {
+            int playerChoice = -1;
+
+            switch (choice)
+            {
+                case "R":
+                    playerChoice = (int)RPSLS.Choice.Rock;
+                    break;
+                case "P":
+                    playerChoice = (int)RPSLS.Choice.Paper;
+                    break;
+                case "S":
+                    playerChoice = (int)RPSLS.Choice.Scissors;
+                    break;
+                case "L":
+                    playerChoice = (int)RPSLS.Choice.Lizard;
+                    break;
+                case "K":
+                    playerChoice = (int)RPSLS.Choice.Spock;
+                    break;
+            }
+            return playerChoice;
+        }
+        //}
     }
 }
--- gameLib/RPSLS.cs
+++ gameLib/RPSLS.cs
@@ -8,9 +8,87 @@ namespace GameLibrary
 {
     public class RPSLS
     {
-        public void SampleClassCall()
+
+        public enum Choice {Rock = 0, Paper = 1, Scissors = 2, Lizard = 3, Spock = 4}
+        public enum Outcome {Win = 10, Lose = 11, Draw = 12}
+
+        // create a constructor
+        // TODO: revised the constructor
+        public RPSLS()
         {
-            Console.WriteLine("hELLO call from GameLibrary");
+
+        }
+        // define player's choice field and property
+        private int playerChoice; 
+        public int PlayerChoice
+        {
+            get { return playerChoice; }
+            set { playerChoice = value; }
+        }
+
+        // get computer's choice (initiate a random number).
+        private static readonly Random num = new Random();
+        private int computerChoice = num.Next(3);
+        public int ComputerChoice
+        {
+            // computer choice is set to read-only
+            get { return computerChoice; }
+        }
+
+        public string DisplayChoice(int choice)
+        {
+            string show = "";
+            switch (choice)
+            {
+                case (int)Choice.Rock:
+                    show = "ROCK";
+                    break;
+                case (int)Choice.Paper:
+                    show = "PAPER";
+                    break;
+                case (int)Choice.Scissors:
+                    show = "SCISSORS";
+                    break;
+                case (int)Choice.Lizard:
+                    show = "LIZARD";
+                    break;
+                case (int)Choice.Spock:
+                    show = "SPOCK";
+                    break;
+            }
+            return show;
+        }
+
+        // display the game outcome
+        public string DisplayOutcome(int outcome)
+        {
+            string result = "";
+            switch (outcome)
+            {
+                case (int)Outcome.Win:
+                    result = "You Win!";
+                    break;
+                case (int)Outcome.Lose:
+                    result = "You Lose!";
+                    break;
+                case (int)Outcome.Draw:
+                    result = "It's a Draw!";
+                    break;
+            }
+            return result;
+        }
+
+         // Determine outcome using 2D Array
+        public int DetermineOutcome()
+        {
+            int result;
+            int[,] selection = new int[5, 5] {  {(int)Outcome.Draw, (int)Outcome.Lose, (int)Outcome.Win,  (int)Outcome.Win, (int)Outcome.Lose},
+                                                {(int)Outcome.Win,  (int)Outcome.Draw, (int)Outcome.Lose, (int)Outcome.Lose,(int)Outcome.Win }, 
+                                                {(int)Outcome.Lose, (int)Outcome.Win,  (int)Outcome.Draw, (int)Outcome.Win, (int)Outcome.Lose},
+                                                {(int)Outcome.Lose, (int)Outcome.Win,  (int)Outcome.Lose, (int)Outcome.Draw,(int)Outcome.Win },
+                                                {(int)Outcome.Win,  (int)Outcome.Lose, (int)Outcome.Win,  (int)Outcome.Lose,(int)Outcome.Draw}};
+            result = selection[PlayerChoice, ComputerChoice];
+            return result;
         }
     }
 }
